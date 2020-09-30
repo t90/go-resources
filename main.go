c32029c600229545f3cef2e1336a2b578d84ba4c
Vladimir Vasiltsov <vasiltsov@gmail.com>
2020-09-30 15:49:34 -0400

    renamed, as it will come that way from go get

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-//go:generate resource $GOPATH/src/resource/res/res.go $GOPATH/src/resource/res_template.gtp
+//go:generate go-resources $GOPATH/src/resource/res/res.go $GOPATH/src/resource/res_template.gtp
 package main
 
 import (
@@ -16,7 +16,7 @@ type ResourceData struct {
 	Data string
 }
 
-func main(){
+func main() {
 	if len(os.Args) < 3 {
 		println("USAGE: resource <output filename.go> <input file 1> [input file 2, 3, ...] ")
 		return
@@ -29,20 +29,20 @@ func main(){
 	}
 	count := len(inputFiles)
 
-	f,err := os.Create(os.Args[1])
+	f, err := os.Create(os.Args[1])
 	defer f.Close()
 
-	if err != nil{
+	if err != nil {
 		panic(err)
 	}
 	var buf bytes.Buffer
 	template := string(res.Resources["res_template.gtp"])
 
-	for ;count > 0; count--{
-		res := <- resources
+	for ; count > 0; count-- {
+		res := <-resources
 		fmt.Fprintf(&buf, "\t\"%s\": {%s},\n", res.Name, res.Data)
 	}
-	template = strings.Replace(template, "%LINE_ITEMS%", buf.String(),-1)
+	template = strings.Replace(template, "%LINE_ITEMS%", buf.String(), -1)
 	f.WriteString(template)
 }
 
@@ -51,12 +51,12 @@ func encodeResource(name string, resources chan ResourceData) {
 		Name: path.Base(name),
 	}
 	data, err := ioutil.ReadFile(name)
-	if err != nil{
+	if err != nil {
 		panic(err)
 	}
 	size := len(data)
-	bytes := make([]string,size)
-	for i,v := range data{
+	bytes := make([]string, size)
+	for i, v := range data {
 		bytes[i] = fmt.Sprintf("0x%x", v)
 	}
 	resData.Data = strings.Join(bytes, ",")
